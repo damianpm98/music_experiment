5be7c0264df35e011670a0529a6ab441e4bb68b7
Damian Perez <damianperez8@gmail.com>
2025-11-04 11:00:14 +0100

    Issue 22: Exception handle for Guitar and Bass lists

--- src/Musician.cpp
+++ src/Musician.cpp
@@ -20,24 +20,70 @@ void Musician::checkInstrumentalSkills() const {
 }
 
 void Musician::addGuitar(const Guitar &guitar) {
-    if (!checkIfExistsInList(_guitars, guitar)) {
-        _guitars.push_back(guitar);
-        std::cout << guitar << " added to musician " << _name << " " << _surname
-                  << "." << std::endl;
-    } else {
-        std::cout << guitar << " already exists for musician " << _name << " "
-                  << _surname << "." << std::endl;
+    try {
+        if (_guitars.size() >= 10) {
+            throw 101;
+        }
+        if (!checkIfExistsInList(_guitars, guitar)) {
+            _guitars.push_back(guitar);
+            std::cout << guitar << " added to musician " << _name << " "
+                      << _surname << "." << std::endl;
+        } else {
+            std::cout << guitar << " already exists for musician " << _name
+                      << " " << _surname << "." << std::endl;
+            throw 201;
+        }
+    } catch (int error) {
+        switch (error) {
+        case 101:
+            std::cout << "Error: Guitar list is full." << std::endl;
+            break;
+
+        case 201:
+            std::cout << "Error: Duplicate guitar." << std::endl;
+            break;
+
+        default:
+            std::cout << "Unknown error occurred." << std::endl;
+            break;
+        }
+    } catch (...) {
+        std::cout << "An unexpected error occurred while adding guitar."
+                  << std::endl;
     }
 }
 
 void Musician::addBass(const Bass &bass) {
-    if (!checkIfExistsInList(_basses, bass)) {
-        _basses.push_back(bass);
-        std::cout << bass << " added to musician " << _name << " " << _surname
-                  << "." << std::endl;
-    } else {
-        std::cout << bass << " already exists for musician " << _name << " "
-                  << _surname << "." << std::endl;
+    try {
+        if (_basses.size() >= 10) {
+            throw 102;
+        }
+        if (!checkIfExistsInList(_basses, bass)) {
+            _basses.push_back(bass);
+            std::cout << bass << " added to musician " << _name << " "
+                      << _surname << "." << std::endl;
+        } else {
+            std::cout << bass << " already exists for musician " << _name << " "
+                      << _surname << "." << std::endl;
+            throw 202;
+        }
+    } catch (int error) {
+        switch (error) {
+        case 102:
+            std::cout << "Error: Bass list is full." << std::endl;
+            break;
+
+        case 202:
+            std::cout << "Error: Duplicate bass." << std::endl;
+            break;
+
+        default:
+            std::cout << "Unknown error occurred." << std::endl;
+            break;
+        }
+    } catch (...) {
+        std::cout << "An unexpected error occurred while adding bass."
+                  << std::endl;
     }
 }
 
